9a8fd596a6901879b9986ac0f330411872762205
Adam Matthiesen <amatthiesen@outlook.com>
2025-01-16 14:07:48 -0800

    Refactor intro tests: simplify username handling and enhance message verification

--- package/tests/intro.test.ts
+++ package/tests/intro.test.ts
@@ -2,19 +2,19 @@ import { describe, expect, it } from 'vitest';
 import { intro } from '../dist/index.js';
 import { setup } from './utils.js';
 
-async function setUsername() {
-	return 'user';
-}
-
-const username = setUsername();
-
 describe('intro', () => {
 	const fixture = setup();
 
 	it('Test Response', async () => {
 		// @ts-expect-error Testing purposes only
-		await intro({ skipBanners: true, version: '0.0.0', username });
+		await intro({ version: '0.0.0', username: 'user' });
+		expect(fixture.hasMessage('studiocms   Interactive CLI')).toBe(true);
+	});
+
+	it('Test Response - Skip Banners', async () => {
+		// @ts-expect-error Testing purposes only
+		await intro({ skipBanners: true, version: '0.0.0', username: 'user' });
 		expect(fixture.length()).toBe(1);
-		expect(fixture.hasMessage('Interactive CLI')).toBe(true);
+		expect(fixture.hasMessage('studiocms   Interactive CLI')).toBe(true);
 	});
 });
